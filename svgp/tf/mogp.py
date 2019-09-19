b437a27e9604db20fcfcc049e2857717531c50df
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-09-19 16:58:04 +0200

    Added new expectation which (hopefully) is more memory-efficient

--- svgp/tf/mogp.py
+++ svgp/tf/mogp.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from svgp.tf.quadrature import expectation, expectation_custom
+from svgp.tf.quadrature import expectation, expectation_map
 from svgp.tf.svgp import compute_qf_mean_cov, compute_kl_term
 from svgp.tf.kl import normal_kl_1d
 from .config import DTYPE
@@ -66,8 +66,9 @@ def compute_objective(x, y, Z, ms, Ls, w_means, w_vars, ks, log_lik_fun,
     # log_liks = tf.map_fn(curried_exp, [tf.transpose(y), tf.transpose(var_out),
     #                                    tf.transpose(m_out)], dtype=DTYPE)
 
-    log_liks = expectation(tf.reshape(y, (-1,)), tf.reshape(var_out, (-1,)),
-                           tf.reshape(m_out, (-1,)), log_lik_fun)
+    log_liks = expectation_map(
+        tf.reshape(y, (-1,)), tf.reshape(var_out, (-1,)),
+        tf.reshape(m_out, (-1,)), log_lik_fun)
 
     total_log_lik = tf.reduce_sum(log_liks)
 
