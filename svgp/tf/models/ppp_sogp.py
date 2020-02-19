fd4c1710f0d0586dd332110319976b3a6e6449d7
Martin Ingram <ingramm@student.unimelb.edu.au>
2020-02-19 14:34:49 -0600

    First version seems to work, but I'm not sure I got the abstraction right

--- svgp/tf/models/ppp_sogp.py
+++ svgp/tf/models/ppp_sogp.py
@@ -19,40 +19,63 @@ from typing import Dict, Optional
 import tensorflow_probability as tfp
 from os.path import join
 from os import makedirs
+from svgp.tf.experimental.kernel_spec import (
+    KernelSpec, update_parameters, get_kernel_fun, calculate_prior_prob)
+import dill
 
 STEP = 0
 
 
-def get_kernel_fun_covs_only(lengthscales, alpha, intercept_sd):
+def get_default_kernel_spec(n_covariates):
 
-    matern_fun = partial(matern_kernel_32, lengthscales=tf.exp(lengthscales),
-                         alpha=tf.exp(alpha))
+    def base_kernel_fun(x1, x2, alpha, lengthscales, intercept_sd,
+                        diag_only=False):
 
-    intercept_fun = partial(bias_kernel, sd=tf.exp(intercept_sd))
+        matern_fun = partial(matern_kernel_32, lengthscales=lengthscales,
+                             alpha=alpha, diag_only=diag_only)
 
-    def compute(x1, x2, diag_only=False):
+        intercept_fun = partial(bias_kernel, sd=intercept_sd,
+                                diag_only=diag_only)
 
-        matern_result = matern_fun(x1, x2, diag_only=diag_only)
-        intercept_result = intercept_fun(x1, x2, diag_only=diag_only)
+        return matern_fun(x1, x2) + intercept_fun(x1, x2)
 
-        return matern_result + intercept_result
+    init_params = {
+        'lengthscales': np.log(np.random.uniform(2., 5., size=n_covariates)),
+        'alpha': np.array(0.),
+        'intercept_sd': np.array(0.)
+    }
 
-    return compute
+    init_params = {x: tf.constant(y.astype(np.float32)) for x, y in
+                   init_params.items()}
+
+    constraints = {x: '+' for x in init_params}
+
+    priors = {
+        'lengthscales': lambda x: tf.reduce_sum(
+            tfp.distributions.Gamma(3, 1/3).log_prob(x))
+    }
+
+    return KernelSpec(
+        base_kernel_fun=base_kernel_fun,
+        parameters=init_params,
+        constraints=constraints,
+        priors=priors
+    )
 
 
-def create_spec(theta):
+def update_specs(theta, kernel_spec):
 
-    kernel_fun = get_kernel_fun_covs_only(
-        theta['lscales'], theta['alpha'], theta['intercept_sd'])
+    kernel_spec = update_parameters(kernel_spec, theta)
+    kernel_fun = get_kernel_fun(kernel_spec)
 
-    cur_spec = InducingPointGPSpecification(
+    gp_spec = InducingPointGPSpecification(
         L_elts=theta['L_elts'],
         mu=theta['mu'],
         kernel_fun=kernel_fun,
         Z=theta['Z']
     )
 
-    return cur_spec
+    return kernel_spec, gp_spec
 
 
 def calculate_objective(X: tf.Tensor, z: tf.Tensor, weights: tf.Tensor,
@@ -75,6 +98,7 @@ def calculate_objective(X: tf.Tensor, z: tf.Tensor, weights: tf.Tensor,
 
 
 def fit(X: np.ndarray, z: np.ndarray, weights: np.ndarray, n_inducing: int,
+        thinning_X: Optional[np.ndarray] = None,
         fit_inducing_using_presences_only: bool = False, verbose: bool = True,
         log_theta_dir: Optional[str] = None, use_berman_turner: bool = False):
     # TODO: Perhaps allow a separate kernel to be placed on bias
@@ -89,13 +113,14 @@ def fit(X: np.ndarray, z: np.ndarray, weights: np.ndarray, n_inducing: int,
 
     n_cov = X.shape[1]
 
-    start_alpha = np.log(1.)
-    start_lscales = np.log(np.random.uniform(2, 5, size=n_cov))
-    start_intercept_sd = np.log(1.)
-    start_kernel_fun = get_kernel_fun_covs_only(
-        tf.constant(start_lscales.astype(np.float32)),
-        tf.constant(np.array(start_alpha).astype(np.float32)),
-        tf.constant(np.array(start_intercept_sd).astype(np.float32)))
+    if thinning_X is None:
+        # We have an "un-thinned" point process. We need only consider the
+        # covariates.
+        init_kernel_spec = get_default_kernel_spec(n_cov)
+    else:
+        assert False, 'thinning not yet implemented!'
+
+    start_kernel_fun = get_kernel_fun(init_kernel_spec)
 
     if fit_inducing_using_presences_only:
         X_to_cluster = X[z > 0, :]
@@ -109,12 +134,12 @@ def fit(X: np.ndarray, z: np.ndarray, weights: np.ndarray, n_inducing: int,
 
     start_theta = {
         'Z': init_Z,
-        'lscales': start_lscales,
-        'intercept_sd': start_intercept_sd,
-        'alpha': start_alpha,
         'mu': init_spec.mu,
         'L_elts': init_spec.L_elts
         }
+
+    # Add the kernel parameters to the optimisation
+    start_theta.update(init_kernel_spec.parameters)
 
     # Make sure they're all the right type
     start_theta = {x: np.array(y).astype(np.float64) for x, y in
@@ -140,17 +165,13 @@ def fit(X: np.ndarray, z: np.ndarray, weights: np.ndarray, n_inducing: int,
 
             theta = reconstruct_tf(flat_theta, summary)
 
-            cur_spec = create_spec(theta)
+            kernel_spec, gp_spec = update_specs(theta, init_kernel_spec)
 
             cur_objective = -calculate_objective(
-                X, z, weights, cur_spec, use_berman_turner=use_berman_turner)
+                X, z, weights, gp_spec, use_berman_turner=use_berman_turner)
 
-            # Add a prior on the lengthscales
-            lscale_prior = tf.reduce_sum(
-                tfp.distributions.Gamma(
-                    3, 1 / 3).log_prob(tf.exp(theta['lscales'])))
-
-            cur_objective = cur_objective - lscale_prior
+            kernel_prior_prob = calculate_prior_prob(kernel_spec)
+            cur_objective = cur_objective - kernel_prior_prob
 
             cur_grad = tape.gradient(cur_objective, flat_theta)
 
@@ -177,21 +198,21 @@ def fit(X: np.ndarray, z: np.ndarray, weights: np.ndarray, n_inducing: int,
                       method='L-BFGS-B', jac=True)
 
     final_flat_theta = result.x.astype(np.float32)
+    final_theta = reconstruct_tf(final_flat_theta, summary)
+    _, final_spec = update_specs(final_theta, init_kernel_spec)
 
-    return reconstruct_tf(final_flat_theta, summary)
+    return final_spec
 
 
-def predict(fit_results: Dict[str, tf.Tensor], X: np.ndarray) -> np.ndarray:
+def predict(gp_spec: InducingPointGPSpecification,
+            X: np.ndarray) -> np.ndarray:
 
-    spec = create_spec(fit_results)
-
-    pred_mean, pred_var = project_to_x(spec, tf.constant(X.astype(np.float32)))
+    pred_mean, pred_var = project_to_x(gp_spec, tf.constant(X.astype(np.float32)))
 
     return pred_mean.numpy(), pred_var.numpy()
 
 
-def save_results(fit_results: Dict[str, tf.Tensor], save_path: str):
+def save_results(fit_spec: InducingPointGPSpecification, save_path: str):
 
-    np_version = {x: y.numpy() for x, y in fit_results.items()}
-
-    np.savez(save_path, **np_version)
+    with open(save_path, 'wb') as f:
+        dill.dump(fit_spec, f)
