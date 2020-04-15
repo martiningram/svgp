(Hopefully) fix alpha scaling

svgp/tf/models/mogp_classifier.py
@@ -33,14 +33,15 @@ class MOGPResult(NamedTuple):
     w_prior_vars: np.ndarray
     intercept_prior_mean: np.float64
     intercept_prior_var: np.float64
+    total_kernel_variance: np.float64
 
 
-def get_kernel_funs(base_kern_fun, lscales):
+def get_kernel_funs(base_kern_fun, lscales, total_variance=6.):
 
     n_l = len(lscales)
 
     kerns = [partial(base_kern_fun, lengthscales=cur_lscales,
-                     alpha=np.sqrt(1 / n_l)) for cur_lscales in
+                     alpha=np.sqrt(total_variance / n_l)) for cur_lscales in
              lscales]
 
     return kerns
@@ -109,7 +110,8 @@ def fit(X: np.ndarray,
         w_mean_prior: Tuple[float, float] = (0, 1),
         bias_mean_prior: Tuple[float, float] = (0, 1),
         random_seed: int = 2,
-        test_run: bool = False) \
+        test_run: bool = False,
+        total_kernel_variance=6.) \
         -> MOGPResult:
 
     np.random.seed(random_seed)
@@ -128,7 +130,8 @@ def fit(X: np.ndarray,
     Z = np.tile(Z, (n_latent, 1, 1))
     Z = Z.astype(np.float32)
 
-    start_kernel_funs = get_kernel_funs(kernel_fun, start_lengthscales)
+    start_kernel_funs = get_kernel_funs(kernel_fun, start_lengthscales,
+                                        total_variance=total_kernel_variance)
 
     init_Ls = np.stack([
         get_initial_values_from_kernel(cur_z, cur_kernel_fun)
@@ -201,7 +204,8 @@ def fit(X: np.ndarray,
 
             Ls = create_ls(theta['L_elts'], n_inducing, n_latent)
 
-            kern_funs = get_kernel_funs(kernel_fun, lscales)
+            kern_funs = get_kernel_funs(kernel_fun, lscales,
+                                        total_variance=total_kernel_variance)
 
             kl = compute_kl_term(theta['mu'], Ls, kern_funs, theta['Z'],
                                  theta['w_means'], w_vars,
@@ -256,7 +260,8 @@ def fit(X: np.ndarray,
         w_prior_means=final_theta['w_prior_mean'],
         w_prior_vars=final_theta['w_prior_var']**2,
         intercept_prior_mean=final_theta['intercept_prior_mean'],
-        intercept_prior_var=final_theta['intercept_prior_var']**2
+        intercept_prior_var=final_theta['intercept_prior_var']**2,
+        total_kernel_variance=total_kernel_variance
     )
 
     return fit_result
@@ -275,7 +280,8 @@ def predict(fit_result: MOGPResult, X_new: np.ndarray):
     base_kern = kern_lookup[fit_result.kernel]
 
     k_funs = get_kernel_funs(base_kern,
-                             fit_result.lengthscales.astype(np.float32))
+                             fit_result.lengthscales.astype(np.float32),
+                             total_variance=fit_result.total_kernel_variance)
 
     pred_mean, pred_var = compute_predictions(
         X_new.astype(np.float32), fit_result.Z.astype(np.float32),
@@ -297,7 +303,8 @@ def predict_latent(fit_result: MOGPResult, X_new: np.ndarray):
     base_kern = kern_lookup[fit_result.kernel]
 
     k_funs = get_kernel_funs(base_kern,
-                             fit_result.lengthscales.astype(np.float32))
+                             fit_result.lengthscales.astype(np.float32),
+                             total_variance=fit_result.total_kernel_variance)
 
     L = create_ls(fit_result.L_elts.astype(np.float32), n_inducing,
                   n_latent)

svgp/tf/models/ppp_mogp.py
@@ -215,11 +215,11 @@ def objective_and_grad(flat_theta, X, X_thin, sp_num, z, weights, summary,
 
 
 def initialise_theta(Z, n_latent, n_cov, n_out, Z_thin=None, init_w_var=1.,
-                     alpha=1.):
+                     log_cov_alpha=1., log_thin_alpha=1.):
 
     start_lscales = np.log(np.random.uniform(
         1., 4., size=(n_latent, n_cov)).astype(np.float32))
-    start_alphas = np.log(np.repeat(alpha, n_latent).astype(np.float32))
+    start_alphas = np.repeat(log_cov_alpha, n_latent).astype(np.float32)
 
     start_k_funs = get_kernel_funs(start_lscales, start_alphas)
 
@@ -253,7 +253,7 @@ def initialise_theta(Z, n_latent, n_cov, n_out, Z_thin=None, init_w_var=1.,
         # Only one shared function for now
         Z_thins = np.tile(Z_thin, (1, 1, 1))
         thin_lscales = np.log(np.random.uniform(2, 5, size=(1, n_cov_thin)))
-        thin_alphas = np.log(np.array([1.]))
+        thin_alphas = np.array([log_thin_alpha])
 
         start_k_funs = get_kernel_funs(
             thin_lscales.astype(np.float32), thin_alphas.astype(np.float32))
@@ -316,7 +316,11 @@ def fit(X: np.ndarray,
     else:
         Z_thin = None
 
-    start_theta = initialise_theta(Z, n_latent, n_cov, n_out, Z_thin=Z_thin)
+    cov_alpha = cov_alpha if cov_alpha is not None else tf.cast(tf.constant(
+        np.log(np.sqrt(2. / n_latent))), tf.float32)
+
+    start_theta = initialise_theta(Z, n_latent, n_cov, n_out, Z_thin=Z_thin,
+                                   log_cov_alpha=cov_alpha)
 
     if fix_thin_inducing:
         # Remove them from the theta dict of parameters to optimise
@@ -335,9 +339,6 @@ def fit(X: np.ndarray,
     opt_state = initialise_state(flat_theta.shape[0])
 
     flat_theta = flat_theta.numpy()
-
-    cov_alpha = cov_alpha if cov_alpha is not None else tf.cast(tf.constant(
-        np.sqrt(2. / n_latent)), tf.float32)
 
     to_optimise = partial(objective_and_grad, n_data=n_data, n_latent=n_latent,
                           summary=summary, use_berman_turner=use_berman_turner,